359874760190b9efa6f8450020de6522dccd5281
romulochuqui <romulochuqui120905@gmail.com>
2020-07-13 15:41:50 -0300

    Primeiro push

---
+++ index.js
@@ -0,0 +1,41 @@
+//O var é utilizado para criar uma variável e impedir que escrevamos muito.
+var olaMundo = "Olá Mundo!";
+console.log(olaMundo);
+
+
+//O let é usado como uma var, mas é usada apenas em um bloco. Depois é descartada e fica inexistente.
+let romulo = "Rômulo Chuqui Bittencourt da Cunha";
+console.log(romulo);
+
+
+//O const não varia. Como o nome diz, é constante
+const money = "CA$H"
+console.log(money);
+
+//String é uma "corda de caracteres", uma linha de comando.
+
+//O Array é um objeto global usado na construção de 'arrays': objetos de alto nível semelhantes a listas. 
+
+//As Funções são trechos de códigos que podem ser reutilizados 
+
+
+
+
+
+
+
+let cor ="verde"; //Nessa linha, se eu escrever "amarelo" no lugar do "verde", por exemplo, a ação mudará para "atenção". 
+
+if (cor === "verde") {
+
+    console.log("siga");
+
+} else if (cor === "amarelo") {
+
+    console.log("atenção")
+
+} else if (cor = "vermelho") {
+
+    console.log("pare");
+
+}
---
+++ index2.js
@@ -0,0 +1,20 @@
+let cor = "azul";
+
+switch (cor) {  //O "switch" é usado para substituir os "if", para não ficar um código enorme.
+
+    case "verde":
+        console.log("siga");
+        break;
+
+    case "amarelo":
+        console.log("atenção");
+        break;
+
+    case "vermelho":
+        console.log("pare");
+        break;
+
+    default:
+        console.log("não sei");
+
+}
